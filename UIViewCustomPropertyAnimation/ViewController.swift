560a8ede3ba02c23cba2dfcf81e7c05d59a6a521
Paul <pbancarel@octo.com>
2019-08-20 18:29:02 +0200

    Initial Commit

---
+++ UIViewCustomPropertyAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIViewCustomPropertyAnimation
+//
+//  Created by Paul Bancarel on 20/08/2019.
+//  Copyright © 2019 Paul Bancarel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
